19d88e68f36eda90e1723e66fc04a9063aed99c2
Nicholas Niro <nik_89@xroutine.net>
2015-10-18 19:05:44 -0400

    runScriptFunc: Rewrote the script completely.
    This new version of the script now supports loading a virtually infinite
    amount of scripts (read libraries) and the added ability to pass values
    to the function to call.
    
    This new feature was added to lift the requirement of the event scripts
    to include the "global" utils.sh script and also to automatically include the
    variables set in the configuration file. In other terms, it is will now
    be possible to access functions from util.sh and variables from the
    configuration file from the event scripts without the requirement of
    including them as they will be automatically included.

--- runScriptFunc.sh
+++ runScriptFunc.sh
@@ -1,21 +1,54 @@
 #! /bin/sh
 
-# this script imports a script and runs only a single function from that script
-# without any arguments and returns the result.
+# this script imports an unlimited amount of scripts and runs only a single function from these scripts
+# and returns the result. It is possible to pass arguments to the script granted you place the function
+# and the arguments between single or double quotes as the last parameter of the script when calling it.
 
-script=$1
-function=$2
+# include all scripts. The last argument is the function name
+typeset -a allScripts
+i=0
+temp=""
+oldTemp=""
+while [ 1 = 1 ]; do
+	oldTemp=$temp
+	if [ "$1" = "" ]; then
+		function=$oldTemp
+		break
+	fi
 
-if [ ! -e $script ]; then
-	exit 1;
-fi
+	if [ "$oldTemp" != "" ]; then
+		allScripts[$i]=$oldTemp
+		i=$((i + 1))
+	fi
 
-. $script
+	temp=$1
 
+	shift 1
+done
+
+#echo "scripts : ${allScripts[@]} - ${#allScripts[@]}"
+#echo "function : $function"
+
+i=0
+while [ $((i < ${#allScripts[@]})) = 1 ]; do
+	if [ ! -e ${allScripts[$i]} ]; then
+		#echo "error script '${allScripts[$i]}\` don't exist"
+		exit 1;
+	fi
+	#echo "including : ${allScripts[$i]}"
+
+	. ${allScripts[$i]}
+	i=$((i + 1))
+done
+
+functionName="`echo \"$function\" | sed -e 's/\([^ ]*\) .*/\1/'`"
+
+echo $functionName
 # we first check if the actual function exists
-if [ "`declare -f | grep $function`" != "" ]; then
+if [ "`declare -f | grep \"$functionName\"`" != "" ]; then
 	echo `$function`
 	exit 0
 else
+	echo "could not find the function"
 	exit 1
 fi
